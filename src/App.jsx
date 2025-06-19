82f789275f37f92a8cace65c00fc6403895e267a
Victoria B. Amponsah <victoriaboatemaa737@gmail.com>
2025-06-19 16:29:48 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import AdsPage from "./pages/AdsPage";
+import Dashboard from "./pages/Dashboard";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+function App() {
+  const router = createBrowserRouter([ 
+   {
+      path: "/",
+      element: <LandingPage/>
+    },
+
+{
+  path:"/ads",
+  element: <AdsPage/>
+},
+
+{
+  path:"/dashboard",
+  element: <Dashboard/>
+},
+
+{
+  path:"/login",
+  element:<LoginPage/>
+},
+
+{
+  path:"/signup",
+  element:<SignupPage/>
+},
+
+
+
+]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
