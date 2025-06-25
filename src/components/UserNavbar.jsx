451d41f5cc89ef8f869c1fba13cfc8b2651757f5
Victoria B. Amponsah <victoriaboatemaa737@gmail.com>
2025-06-25 16:46:41 +0000

    editing

--- src/components/UserNavbar.jsx
+++ src/components/UserNavbar.jsx
@@ -3,21 +3,27 @@ import { useNavigate } from "react-router";
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <nav className="bg-gray-800 fixed top-0 left-0 w-full z-50 text-white px-4 py-3 flex flex-col md:flex-row items-start md:items-center justify-between">
+    <nav className="bg-pink-200 fixed top-0 left-0 w-full z-50 text-white  px-4 py-3 flex flex-col md:flex-row items-start md:items-center justify-between">
       {/* Left: Links */}
       <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6"></div>
 
       {/* Right: Buttons */}
-      <div className="mt-3 md:mt-0 flex flex-col md:flex-row items-start md:items-center space-y-2 md:space-y-0 md:space-x-4">
-        <a href="/home" className="hover:underline">
+      <div className="mt-3 md:mt-0 flex flex-col md:flex-row items-start md:items-center space-y-2 md:space-y-0 md:space-x-8 mr-10 ">
+        <a href="/home"
+        onClick={() => navigate("/user-ads")}
+        className="underline hover:bg-pink-300  text-pink-900 font-extrabold decoration-1">
           Home
         </a>
-        <a href="/about" className="hover:underline">
+        <a
+          href="/about"
+          onClick={() => navigate("/about")}
+          className="underline hover:bg-pink-300  text-pink-900 font-extrabold decoration-1"
+        >
           About Us
         </a>
         <button
           onClick={() => navigate("/signup")}
-          className="bg-orange-600 hover:bg-orange-400 px-4 py-2 rounded"
+          className="bg-pink-500 cursor-pointer hover:bg-pink-700  font-semibold px-4 py-2 rounded-lg   shadow-md transition duration-300 ease-in-out"
         >
           Create Account
         </button>
@@ -26,7 +32,7 @@ const Navbar = () => {
         <div className="relative">
           <button
             onClick={() => navigate("/login")}
-            className="bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded"
+            className="bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded cursor-pointer "
           >
             Login
           </button>
